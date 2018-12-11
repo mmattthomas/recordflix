5abf7659bd2ad0c57e762fda0b18b79e493f881e
Matt Thomas <mmattthomas@gmail.com>
2018-12-11 08:22:40 -0800

    config changes for assets JS in production

--- app/assets/javascripts/track_likes.js
+++ app/assets/javascripts/track_likes.js
@@ -1,36 +1,34 @@
-// $(document).ready(function() {
-//     $(".newvote").click(function() {
-//         var track_id = $(this).attr('id');
-//         //alert("newvote clicked");
-//         $.ajax({
-//         type: "PUT",
-//         url: 'like/' + track_id,
-//         success: function() {
+$(document).ready(function() {
+    $('[data-js-like-link]').click(function(event){
 
-//             // change image or something
-//             $(this).html = "<i class='fa fa-heart icon redheart'></i>";
-            
-//         }
-//         })
-//     })
-// });
+        var track_id = $(this).attr('data-js-id');
+        var liked_by_me = $(this).attr('data-js-liked-by-me');
+        
+        //var verb = (liked_by_me && liked_by_me == 'true' ? "unlike" : "like");
+        var verb = "";
+        if (liked_by_me == 'true') {
+            verb = "unlike"
+        }
+        if (liked_by_me == 'false') {
+            verb = "like"
+        }
+        $.ajax({
+            method: 'put',
+            url: verb + '/' + track_id,
+            success: function(result){
+                var $a = $('[data-js-id="' + track_id + '"]');            
+                if (liked_by_me === 'false') {
+                    $a.attr('data-js-liked-by-me', 'true')
+                    $a.html("<i class='fa fa-heart icon' style='color:red;'></i> &nbsp; " + result.count);
+                }
+                else {
+                    $a.attr('data-js-liked-by-me', 'false');
+                    $a.html("<i class='fa fa-heart icon'></i> &nbsp; " + result.count);
+                }
+            }
+        });
 
-/* yet a new plan
-
-So this tangled mess is yet again not working perfectly...
-
-start over, clear out all existing js/coffeescript
-clear out link helpers
-
-start slow...
-add button w/ proper classs definition
-add a click event listener with jquery (remember event.prevetn default?)
-https://www.engineyard.com/blog/using-jquery-with-rails-how-to
-have it just throw an alert
-then have it include the track ID in the alert
-then have it make an ajax call (google examples of this) (to make hte update)
-then have it be able to make an unlike OR like call
-then have it process the response (alert)
-then have it update the element(s) based on the response
-
-*/
+        
+        event.preventDefault(); 
+    });
+});
